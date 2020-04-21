1d3cdd8310fe1bd5a303a214bbd250f5d148a5b5
Jeongfeel <eve8108-@hanmail.net>
2020-04-21 19:26:04 +0900

    solved by jp.

---
+++ bfs/Baekjoon_1600_말이 되고픈 원숭이.cpp
@@ -0,0 +1,112 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	1600
+	problem name	:	말이 되고픈 원숭이
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+#include <utility>
+#include <tuple>
+#include <queue>
+using namespace std;
+
+const int MAX_NUMBER = 200;
+const int MAX_MOVING_OF_HORSE = 30 + 1;
+const int PALIN = 0;
+const int OBSTACLE = 1;
+const int COUNT_OF_MONKEY_MOVE = 4;
+const int COUNT_OF_HORSE_MOVE = 8;
+
+int moveXOfMonkey[] = { 1,-1,0,0 };
+int moveYOfMonkey[] = { 0,0,1,-1 };
+int moveXOfHorse[] = { -2,-2,-1,-1,1,1,2,2 };
+int moveYOfHorse[] = { 1,-1,2,-2,2,-2,1,-1 };
+
+int world[MAX_NUMBER][MAX_NUMBER];
+bool isVisitedMonkeyMove[MAX_MOVING_OF_HORSE][MAX_NUMBER][MAX_NUMBER];
+bool isVisitedHorseMove[MAX_MOVING_OF_HORSE][MAX_NUMBER][MAX_NUMBER];
+
+void checkMonkeyMove(int xCoordinate, int yCoordinate);
+
+int K;
+int W, H;
+int main() {
+	cin >> K;
+	cin >> W >> H;
+	for (int i = 0; i < H; i++) {
+		for(int j = 0; j < W; j++) {
+			cin >> world[i][j];
+		}
+	}
+
+	checkMonkeyMove(0, 0);
+}
+
+void checkMonkeyMove(int xCoordinate, int yCoordinate) {
+	queue <tuple <pair<int, int>, int, int >> monkeyPossibleMove;
+	monkeyPossibleMove.push(make_tuple(make_pair(xCoordinate, yCoordinate), K, 0));
+
+	isVisitedMonkeyMove[K][xCoordinate][yCoordinate] = true;
+	isVisitedHorseMove[K][xCoordinate][yCoordinate] = true;
+
+	while (!monkeyPossibleMove.empty()) {
+		int currentXCoordinate = get<0>(monkeyPossibleMove.front()).first;
+		int currentYCoordinate = get<0>(monkeyPossibleMove.front()).second;
+		int currentHorseMoving = get<1>(monkeyPossibleMove.front());
+		int currentMovingCount = get<2>(monkeyPossibleMove.front());
+
+		monkeyPossibleMove.pop();
+
+		if (currentXCoordinate == H - 1 && currentYCoordinate == W - 1) {
+			cout << currentMovingCount << '\n';
+			return;
+		}
+
+		for (int i = 0; i < COUNT_OF_MONKEY_MOVE; i++) {
+			int nextXCoordinate = currentXCoordinate + moveXOfMonkey[i];
+			int nextYCoordinate = currentYCoordinate + moveYOfMonkey[i];
+
+			if (nextXCoordinate < 0 || nextXCoordinate == H || nextYCoordinate < 0 || nextYCoordinate == W) {
+				continue;
+			}
+
+			if (world[nextXCoordinate][nextYCoordinate] == OBSTACLE) {
+				continue;
+			}
+
+			if (isVisitedMonkeyMove[currentHorseMoving][nextXCoordinate][nextYCoordinate] == true) {
+				continue;
+			}
+
+			isVisitedMonkeyMove[currentHorseMoving][nextXCoordinate][nextYCoordinate] = true;
+			monkeyPossibleMove.push(make_tuple(make_pair(nextXCoordinate, nextYCoordinate), currentHorseMoving, currentMovingCount + 1));
+		}
+
+		if (currentHorseMoving == 0) {
+			continue;
+		}
+
+		for (int i = 0; i < COUNT_OF_HORSE_MOVE; i++) {
+			int nextXCoordinate = currentXCoordinate + moveXOfHorse[i];
+			int nextYCoordinate = currentYCoordinate + moveYOfHorse[i];
+
+			if (nextXCoordinate < 0 || nextXCoordinate >= H || nextYCoordinate < 0 || nextYCoordinate >= W) {
+				continue;
+			}
+
+			if (world[nextXCoordinate][nextYCoordinate] == OBSTACLE) {
+				continue;
+			}
+
+			if (isVisitedHorseMove[currentHorseMoving][nextXCoordinate][nextYCoordinate] == true) {
+				continue;
+			}
+
+			isVisitedHorseMove[currentHorseMoving][nextXCoordinate][nextYCoordinate] = true;
+			monkeyPossibleMove.push(make_tuple(make_pair(nextXCoordinate, nextYCoordinate), currentHorseMoving - 1, currentMovingCount + 1));
+		}
+	}
+
+	cout << "-1" << '\n';
+}
---
+++ bfs/Baekjoon_3184_양.cpp
@@ -0,0 +1,95 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	3184
+	problem name	:	양
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+using namespace std;
+
+const int MAX_NUMBER = 250;
+const char EMPTY_FIELD = '.';
+const char FENCE = '#';
+const char WOLF = 'v';
+const char SHEEP = 'o';
+
+char garden[MAX_NUMBER][MAX_NUMBER];
+bool isVisitedGarden[MAX_NUMBER][MAX_NUMBER];
+
+int moveXCoordinate[] = { 1,-1,0,0 };
+int moveYCoordinate[] = { 0,0,1,-1 };
+
+void checkGarden(int xCoordinate, int yCoordinate);
+
+int countOfSheep;
+int countOfWolf;
+
+int R, C;
+int main() {
+	cin >> R >> C;
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> garden[i][j];
+		}
+	}
+
+	int countOfSurviveSheep = 0;
+	int countOfSurviveWolf = 0;
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			if (garden[i][j] == FENCE) {
+				continue;
+			}
+
+			if (isVisitedGarden[i][j] == true) {
+				continue;
+			}
+
+			countOfSheep = 0;
+			countOfWolf = 0;
+			
+			checkGarden(i, j);
+
+			if (countOfSheep > countOfWolf) {
+				countOfSurviveSheep += countOfSheep;
+			}
+			else if (countOfSheep <= countOfWolf) {
+				countOfSurviveWolf += countOfWolf;
+			}
+		}
+	}
+
+	cout << countOfSurviveSheep << " " << countOfSurviveWolf << '\n';
+}
+
+void checkGarden(int xCoordinate, int yCoordinate) {
+	isVisitedGarden[xCoordinate][yCoordinate] = true;
+
+	if (garden[xCoordinate][yCoordinate] == SHEEP) {
+		countOfSheep += 1;
+	}
+	else if (garden[xCoordinate][yCoordinate] == WOLF) {
+		countOfWolf += 1;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int nextXCoordinate = xCoordinate + moveXCoordinate[i];
+		int nextYCoordinate = yCoordinate + moveYCoordinate[i];
+
+		if (nextXCoordinate < 0 || nextXCoordinate == R || nextYCoordinate < 0 || nextYCoordinate == C) {
+			continue;
+		}
+
+		if (garden[nextXCoordinate][nextYCoordinate] == FENCE) {
+			continue;
+		}
+
+		if (isVisitedGarden[nextXCoordinate][nextYCoordinate] == true) {
+			continue;
+		}
+
+		checkGarden(nextXCoordinate, nextYCoordinate);
+	}
+}
