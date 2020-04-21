3465e3edbf25252d6ed46e38bfc6b4108eea6249
Jeongfeel <eve8108-@hanmail.net>
2020-04-21 23:22:06 +0900

    solved by jp.

---
+++ brute force/Baekjoon_1051_숫자 정사각형.cpp
@@ -0,0 +1,63 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	1051
+	problem name	:	숫자 정사각형
+	algorithm	:	brute force
+*/
+
+#include <iostream>
+using namespace std;
+
+const int MAX_NUMBER = 50;
+
+char rectangle[MAX_NUMBER][MAX_NUMBER];
+
+void checkLargeSquare();
+
+int N, M;
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> rectangle[i][j];
+		}
+	}
+
+	checkLargeSquare();
+}
+
+void checkLargeSquare() {
+	int currentLargeSquare = 1;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int currentNumber = rectangle[i][j] - '0';
+			
+			int startNumber = 1;
+			while (true) {
+				if (i + startNumber == N) {
+					break;
+				}
+				if (j + startNumber == M) {
+					break;
+				}
+
+				if ((rectangle[i][j + startNumber] - '0') != currentNumber || (rectangle[i + startNumber][j] - '0') != currentNumber || (rectangle[i + startNumber][j + startNumber] - '0') != currentNumber) {
+					startNumber += 1;
+					continue;
+				}
+
+				if (currentLargeSquare > startNumber + 1) {
+					startNumber += 1;
+					continue;
+				}
+
+				currentLargeSquare = startNumber + 1;
+				startNumber += 1;
+			}
+		}
+	}
+
+	cout << currentLargeSquare * currentLargeSquare << '\n';
+}
---
+++ dynamic programming/Baekjoon_14002_가장 긴 증가하는 부분 수열 4.cpp
@@ -0,0 +1,87 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	14002
+	problem name	:	가장 긴 증가하는 부분 수열 4
+	algorithm	:	dynamic programming
+*/
+
+#include <iostream>
+#include <utility>
+#include <stack>
+using namespace std;
+
+const int MAX_NUMBER = 1000;
+
+int A[MAX_NUMBER];
+int countOfIncerasingArray[MAX_NUMBER];
+int parentNode[MAX_NUMBER];
+
+void initCountOfIncerasingArray();
+void checkIncreasingArray();
+void checkMostLongestIncreasingArray();
+
+int N;
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	initCountOfIncerasingArray();
+	checkIncreasingArray();
+	checkMostLongestIncreasingArray();
+}
+
+void initCountOfIncerasingArray() {
+	for (int i = 0; i < N; i++) {
+		countOfIncerasingArray[i] = 1;
+		parentNode[i] = i;
+	}
+}
+
+void checkIncreasingArray() {
+	for (int i = 0; i < N; i++) {
+		int currentNumber = A[i];
+		for (int j = i + 1; j < N; j++) {
+			if (currentNumber >= A[j]) {
+				continue;
+			}
+
+			if (countOfIncerasingArray[j] >= countOfIncerasingArray[i] + 1) {
+				continue;
+			}
+	
+			countOfIncerasingArray[j] = countOfIncerasingArray[i] + 1;
+			parentNode[j] = i;
+		}
+	}
+}
+
+void checkMostLongestIncreasingArray() {
+	int longestLength = 0;
+	int longestLengthIndex = 0;
+	for (int i = 0; i < N; i++) {
+		if (countOfIncerasingArray[i] <= longestLength) {
+			continue;
+		}
+
+		longestLength = countOfIncerasingArray[i];
+		longestLengthIndex = i;
+	}
+
+	stack<int> parentNodeStack;
+
+	while (longestLengthIndex != parentNode[longestLengthIndex]) {
+		parentNodeStack.push(A[longestLengthIndex]);
+
+		longestLengthIndex = parentNode[longestLengthIndex];
+	}
+	parentNodeStack.push(A[longestLengthIndex]);
+
+	cout << longestLength << '\n';
+	while (!parentNodeStack.empty()) {
+		cout << parentNodeStack.top() << " ";
+		parentNodeStack.pop();
+	}
+}
