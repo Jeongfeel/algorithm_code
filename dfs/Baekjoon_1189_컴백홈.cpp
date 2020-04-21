b8a515dcf47a7f53028d251ea0a834be5359d5db
Jeongfeel <eve8108-@hanmail.net>
2020-04-21 13:11:24 +0900

    solved by jp.

---
+++ dfs/Baekjoon_1189_컴백홈.cpp
@@ -0,0 +1,66 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	1189
+	problem name	:	컴백홈
+	algorithm	:	dfs
+*/
+
+#include <iostream>
+using namespace std;
+
+const int MAX_NUMBER = 5;
+
+int moveX[] = { 1,-1,0,0 };
+int moveY[] = { 0,0,1,-1 };
+
+char map[MAX_NUMBER][MAX_NUMBER];
+bool isVisitedMap[MAX_NUMBER][MAX_NUMBER];
+
+void checkMap(int xCoordinate, int yCoordinate, int currentWay);
+
+int	way = 0;
+
+int R, C, K;
+int main() {
+	cin >> R >> C >> K;
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	checkMap(R-1, 0, 1);
+	cout << way << '\n';
+}
+
+void checkMap(int xCoordinate, int yCoordinate, int currentWay) {
+	isVisitedMap[xCoordinate][yCoordinate] = true;
+	
+	if (xCoordinate == 0 && yCoordinate == C - 1) {
+		if (currentWay == K) {
+			way += 1;
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int nextXCoordinate = xCoordinate + moveX[i];
+		int nextYCoordinate = yCoordinate + moveY[i];
+
+		if (nextXCoordinate < 0 || nextXCoordinate == R || nextYCoordinate < 0 || nextYCoordinate == C) {
+			continue;
+		}
+
+		if (isVisitedMap[nextXCoordinate][nextYCoordinate] == true) {
+			continue;
+		}
+
+		if (map[nextXCoordinate][nextYCoordinate] == 'T') {
+			continue;
+		}
+
+		checkMap(nextXCoordinate, nextYCoordinate, currentWay + 1);
+	}
+
+	isVisitedMap[xCoordinate][yCoordinate] = false;
+}
---
+++ math/Baekjoon_1212_8진수 2진수.cpp
@@ -0,0 +1,60 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	1212
+	problem name	:	8진수 2진수
+	algorithm	:	math
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string makeDigitNumber(int number);
+string checkHeaderZero(string digitNumber);
+int main() {
+	string number;
+	cin >> number;
+
+	string answer = "";
+
+	if (number == "0") {
+		cout << "0" << '\n';
+		return 0;
+	}
+
+	for (int i = 0; i < number.length(); i++) {
+		int digitNumber = number[i] - '0';
+		answer += makeDigitNumber(digitNumber);
+	}
+
+	answer = checkHeaderZero(answer);
+
+	cout << answer << '\n';
+}
+
+string makeDigitNumber(int number) {
+	string digitNumber = "";
+
+	int firstNumber = number % 2;
+	int secondNumber = number / 2 % 2;
+	int thirdNumber = number / 2 / 2;
+
+	digitNumber = to_string(thirdNumber) + to_string(secondNumber) + to_string(firstNumber);
+
+	return digitNumber;
+}
+
+string checkHeaderZero(string digitNumber) {
+	int index = 0;
+
+	for (int i = 0; i < digitNumber.length(); i++) {
+		if (digitNumber[i] == '0') {
+			index += 1;
+			continue;
+		}
+
+		break;
+	}
+
+	return digitNumber.substr(index, digitNumber.length());
+}
