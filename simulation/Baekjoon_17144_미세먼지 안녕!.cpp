709e657859f209d5c3675f1734a8640c9ace2ec2
Jeongfeel <eve8108-@hanmail.net>
2020-04-23 14:42:38 +0900

    solved by jp.

---
+++ simulation/Baekjoon_17144_미세먼지 안녕!.cpp
@@ -0,0 +1,169 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	17144
+	problem name	:	미세먼지 안녕!
+	algorithm	:	simulation
+*/
+
+#include <iostream>
+#include <utility>
+using namespace std;
+
+const int MAX_NUMBER = 50;
+const int CLEAN_ROBOT = -1;
+const int NO_DUST = 0;
+
+int moveDustXCoordinate[] = { 1,-1,0,0 };
+int moveDustYCoordinate[] = { 0,0,1,-1 };
+
+
+int room[MAX_NUMBER + 1][MAX_NUMBER + 1];
+int newRoom[MAX_NUMBER + 1][MAX_NUMBER + 1];
+
+void checkDustCount();
+pair<int,int> findCleanRobot();
+void initNewRoom(int firstCleanRobotPosition, int secondCleanRobotPosition);
+void spreadDust();
+void activateFirstCleanRobot(int firstCleanRobotPosition);
+void activateSecondCleanRobot(int secondCleanRobotPosition);
+void printCountOfDust();
+
+int R, C, T;
+int main() {
+	cin >> R >> C >> T;
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			cin >> room[i][j];
+		}
+	}
+
+	checkDustCount();
+}
+
+void checkDustCount() {
+	pair<int, int> positionOfCleanRobot = findCleanRobot();
+
+	int firstCleanRobotPosition = positionOfCleanRobot.first;
+	int secondCleanRobotPosition = positionOfCleanRobot.second;
+
+	while (T--) {
+		initNewRoom(firstCleanRobotPosition, secondCleanRobotPosition);
+		spreadDust();
+		activateFirstCleanRobot(firstCleanRobotPosition);
+		activateSecondCleanRobot(secondCleanRobotPosition);
+	}
+
+	printCountOfDust();
+}
+
+pair<int, int> findCleanRobot() {
+	for (int i = 1; i <= R; i++) {
+		if (room[i][1] != CLEAN_ROBOT) {
+			continue;
+		}
+
+		return make_pair(i, i + 1);
+	}
+}
+
+void initNewRoom(int firstCleanRobotPosition, int secondCleanRobotPosition) {
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			newRoom[i][j] = NO_DUST;
+		}
+	}
+
+	newRoom[firstCleanRobotPosition][1] = CLEAN_ROBOT;
+	newRoom[secondCleanRobotPosition][1] = CLEAN_ROBOT;
+}
+
+void spreadDust() {
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			if (room[i][j] == NO_DUST) {
+				continue;
+			}
+
+			if (room[i][j] == CLEAN_ROBOT) {
+				continue;
+			}
+
+			int quantityOfDust = room[i][j] / 5;
+			int quantityOfSpreadDust = 0;
+
+			for (int k = 0; k < 4; k++) {
+				int nextDustXCoordinate = i + moveDustXCoordinate[k];
+				int nextDustYCoordinate = j + moveDustYCoordinate[k];
+
+				if (nextDustXCoordinate == 0 || nextDustXCoordinate > R || nextDustYCoordinate == 0 || nextDustYCoordinate > C) {
+					continue;
+				}
+
+				if (room[nextDustXCoordinate][nextDustYCoordinate] == CLEAN_ROBOT) {
+					continue;
+				}
+
+				newRoom[nextDustXCoordinate][nextDustYCoordinate] += quantityOfDust;
+				quantityOfSpreadDust += quantityOfDust;
+			}
+
+			newRoom[i][j] += (room[i][j] - quantityOfSpreadDust);
+		}
+	}
+
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			room[i][j] = newRoom[i][j];
+		}
+	}
+}
+
+void activateFirstCleanRobot(int firstCleanRobotPosition) {
+	for (int i = firstCleanRobotPosition - 2; i >= 1; i--) {
+		room[i + 1][1] = room[i][1];
+	}
+	for (int i = 2; i <= C; i++) {
+		room[1][i - 1] = room[1][i];
+	}
+	for (int i = 1; i < firstCleanRobotPosition; i++) {
+		room[i][C] = room[i + 1][C];
+	}
+	for (int i = C; i > 2; i--) {
+		room[firstCleanRobotPosition][i] = room[firstCleanRobotPosition][i - 1];
+	}
+	room[firstCleanRobotPosition][2] = NO_DUST;
+}
+
+void activateSecondCleanRobot(int secondCleanRobotPosition) {
+	for (int i = secondCleanRobotPosition + 2; i <= R; i++) {
+		room[i - 1][1] = room[i][1];
+	}
+	for (int i = 2; i <= C; i++) {
+		room[R][i - 1] = room[R][i];
+	}
+	for (int i = R; i > secondCleanRobotPosition; i--) {
+		room[i][C] = room[i - 1][C];
+	}
+	for (int i = C; i > 2; i--) {
+		room[secondCleanRobotPosition][i] = room[secondCleanRobotPosition][i - 1];
+	}
+	room[secondCleanRobotPosition][2] = NO_DUST;
+}
+
+void printCountOfDust() {
+	int countOfDust = 0;
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			if (room[i][j] == NO_DUST) {
+				continue;
+			}
+			if (room[i][j] == CLEAN_ROBOT) {
+				continue;
+			}
+
+			countOfDust += room[i][j];
+		}
+	}
+
+	cout << countOfDust << '\n';
+}
---
+++ simulation/Baekjoon_17822_원판 돌리기.cpp
@@ -0,0 +1,206 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	17822
+	problem name	:	원판 돌리기
+	algorithm	:	simulation
+*/
+
+#include <iostream>
+using namespace std;
+
+const int MAX_NUMBER_OF_CIRCLE = 50;
+const int BLANK = 0;
+const int CLOCK_WISE = 0;
+const int COUNTER_CLOCK_WISE = 1;
+
+int circle[MAX_NUMBER_OF_CIRCLE + 1][MAX_NUMBER_OF_CIRCLE + 1];
+int nextCircle[MAX_NUMBER_OF_CIRCLE + 1][MAX_NUMBER_OF_CIRCLE + 1];
+
+void spinCircle(int indexOfCircle, int direction, int spinNumber);
+void checkAdjacentNumberOfCircle();
+void initNextCircle();
+void spinCircleClockWise(int indexOfCircle, int spinNumber);
+void spinCircleCounterClockWise(int indexOfCircle, int spinNumber);
+void printSumOfRemainNumber();
+
+int N, M, T;
+int main() {
+	cin >> N >> M >> T;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			cin >> circle[i][j];
+		}
+	}
+
+	for (int i = 0; i < T; i++) {
+		int x, d, k;
+		cin >> x >> d >> k;
+
+		if (x != 1) {
+			spinCircle(x, d, k);
+		}
+
+		checkAdjacentNumberOfCircle();
+	}
+
+	printSumOfRemainNumber();
+}
+
+void spinCircle(int indexOfCircle, int direction, int spinNumber) {
+	initNextCircle();
+
+	if (direction == CLOCK_WISE) {
+		spinCircleClockWise(indexOfCircle, spinNumber);
+	}
+	else if (direction == COUNTER_CLOCK_WISE) {
+		spinCircleCounterClockWise(indexOfCircle, spinNumber);
+	}
+}
+
+void checkAdjacentNumberOfCircle() {
+	bool isDeleteNumber = false;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (circle[i][j] == -1) {
+				continue;
+			}
+
+			int firstCircleIndex = j;
+			int secondCircleIndex = j + 1;
+			if (secondCircleIndex > M) {
+				secondCircleIndex = 1;
+			}
+
+			if (circle[i][firstCircleIndex] == circle[i][secondCircleIndex]) {
+				nextCircle[i][firstCircleIndex] = -1;
+				nextCircle[i][secondCircleIndex] = -1;
+
+				isDeleteNumber = true;
+			}
+		}
+	}
+	
+	for (int i = 1; i <= M; i++) {
+		for (int j = 1; j < N; j++) {
+			if (circle[j][i] == -1) {
+				continue;
+			}
+
+			int firstCircleIndex = j;
+			int secondCircleIndex = j + 1;
+
+			if (circle[firstCircleIndex][i] == circle[secondCircleIndex][i]) {
+				nextCircle[firstCircleIndex][i] = -1;
+				nextCircle[secondCircleIndex][i] = -1;
+
+				isDeleteNumber = true;
+			}
+		}
+	}
+	
+	if (isDeleteNumber == true) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				if (nextCircle[i][j] == -1) {
+					circle[i][j] = -1;
+				}
+			}
+		}
+	}
+	else if (isDeleteNumber == false) {
+		int countOfExistNumber = 0;
+		float averageNumber = 0;
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				if (circle[i][j] == -1) {
+					continue;
+				}
+
+				countOfExistNumber += 1;
+				averageNumber += circle[i][j];
+			}
+		}
+
+		averageNumber /= countOfExistNumber;
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				if (circle[i][j] == -1) {
+					continue;
+				}
+
+				if (circle[i][j] > averageNumber) {
+					circle[i][j] -= 1;
+				}
+				else if (circle[i][j] < averageNumber) {
+					circle[i][j] += 1;
+				}
+			}
+		}
+	}
+}
+
+void initNextCircle() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			nextCircle[i][j] = BLANK;
+		}
+	}
+}
+
+void spinCircleClockWise(int indexOfCircle, int spinNumber) {
+	for (int i = indexOfCircle; i <= N; i += indexOfCircle) {
+		for (int j = 1; j <= M; j++) {
+			int nextPosition = j + spinNumber;
+
+			nextPosition %= M;
+			if (nextPosition == 0) {
+				nextPosition = M;
+			}
+			nextCircle[i][nextPosition] = circle[i][j];
+		}
+
+		for (int j = 1; j <= M; j++) {
+			circle[i][j] = nextCircle[i][j];
+		}
+	}
+}
+
+void spinCircleCounterClockWise(int indexOfCircle, int spinNumber) {
+	for (int i = indexOfCircle; i <= N; i += indexOfCircle) {
+		for (int j = 1; j <= M; j++) {
+			int nextPosition = spinNumber % M;
+			nextPosition = M - nextPosition;
+			nextPosition = j + nextPosition;
+			nextPosition %= M;
+
+			if (nextPosition == 0) {
+				nextPosition = M;
+			}
+
+			nextCircle[i][nextPosition] = circle[i][j];
+		}
+
+		for (int j = 1; j <= M; j++) {
+			circle[i][j] = nextCircle[i][j];
+		}
+	}
+}
+
+void printSumOfRemainNumber() {
+	int sumOfRemainNumber = 0;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (circle[i][j] == -1) {
+				continue;
+			}
+
+			sumOfRemainNumber += circle[i][j];
+		}
+	}
+
+	cout << sumOfRemainNumber << '\n';
+}
