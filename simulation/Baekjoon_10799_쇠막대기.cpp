3387efd4107aadbce9984d8a1ac55b62c1f62e2e
Jeongfeel <eve8108-@hanmail.net>
2020-05-04 14:42:42 +0900

    solved by jp.

---
+++ simulation/Baekjoon_10799_쇠막대기.cpp
@@ -0,0 +1,35 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	10799
+	problem name	:	쇠막대기
+	algorithm	:	simulation
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string ironStick;
+	cin >> ironStick;
+
+	int countOfIronStick = 0;
+	int countOfSerparteStick = 0;
+	
+	for (int i = 0; i < ironStick.size(); i++) {
+		if (ironStick[i] == '(') {
+			countOfIronStick += 1;
+		}
+		else if (ironStick[i] == ')') {
+			countOfIronStick -= 1;
+			if (ironStick[i - 1] == '(') {
+				countOfSerparteStick += countOfIronStick;
+			}
+			else {
+				countOfSerparteStick += 1;
+			}
+		}
+	}
+
+	cout << countOfSerparteStick << '\n';
+}
