4b08b9cea287a4c0c3656d95eb73b82e3389c95b
Jeongfeel <eve8108-@hanmail.net>
2020-05-06 20:33:51 +0900

    solved by jp.

---
+++ bfs/Baekjoon_12851_숨바꼭질2.cpp
@@ -0,0 +1,95 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	12851
+	problem name	:	숨바꼭질2
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+#include <queue>
+#include <utility>
+using namespace std;
+
+const int MAX_NUMBER_OF_STAIR = 100000;
+
+int visitedStairTime[MAX_NUMBER_OF_STAIR + 1];
+
+int findWayOfArriveDestination(queue<pair<int, int>> placeOfSubinAndTimeQueue, int destinationTime);
+
+void initIsVistedStair();
+
+int placeOfSubin, placeOfSister;
+int main() {
+	cin >> placeOfSubin >> placeOfSister;
+
+	queue<pair<int, int>> placeOfSubinAndTimeQueue;
+	placeOfSubinAndTimeQueue.push(make_pair(placeOfSubin, 0));
+
+	initIsVistedStair();
+
+	int destinationTime, wayOfArriveDestination;
+	while (!placeOfSubinAndTimeQueue.empty()) {
+		int currentPlaceOfSubin = placeOfSubinAndTimeQueue.front().first;
+		int currentTime = placeOfSubinAndTimeQueue.front().second;
+
+		placeOfSubinAndTimeQueue.pop();
+		if (currentPlaceOfSubin == placeOfSister) {
+			destinationTime = currentTime;
+			wayOfArriveDestination = findWayOfArriveDestination(placeOfSubinAndTimeQueue, currentTime);
+			break;
+		}
+		else {
+			if (currentPlaceOfSubin + 1 <= MAX_NUMBER_OF_STAIR && visitedStairTime[currentPlaceOfSubin + 1] >= currentTime + 1) {
+				int nextPlaceOfSubin = currentPlaceOfSubin + 1;
+				int nextTime = currentTime + 1;
+
+				placeOfSubinAndTimeQueue.push(make_pair(nextPlaceOfSubin, nextTime));
+				visitedStairTime[nextPlaceOfSubin] = nextTime;
+			}
+
+			if (currentPlaceOfSubin - 1 >= 0 && visitedStairTime[currentPlaceOfSubin - 1] >= currentTime + 1) {
+				int nextPlaceOfSubin = currentPlaceOfSubin - 1;
+				int nextTime = currentTime + 1;
+
+				placeOfSubinAndTimeQueue.push(make_pair(nextPlaceOfSubin, nextTime));
+				visitedStairTime[nextPlaceOfSubin] = nextTime;
+			}
+
+			if (currentPlaceOfSubin * 2 <= MAX_NUMBER_OF_STAIR && visitedStairTime[currentPlaceOfSubin * 2] >= currentTime + 1) {
+				int nextPlaceOfSubin = currentPlaceOfSubin * 2;
+				int nextTime = currentTime + 1;
+
+				placeOfSubinAndTimeQueue.push(make_pair(nextPlaceOfSubin, nextTime));
+				visitedStairTime[nextPlaceOfSubin] = nextTime;
+			}
+		}
+	}
+
+	cout << destinationTime << '\n' << wayOfArriveDestination << '\n';
+}
+
+int findWayOfArriveDestination(queue<pair<int, int>> placeOfSubinAndTimeQueue, int destinationTime) {
+	int wayOfArriveDestination = 1;
+	while (!placeOfSubinAndTimeQueue.empty()) {
+		if (placeOfSubinAndTimeQueue.front().first != placeOfSister) {
+			placeOfSubinAndTimeQueue.pop();
+			continue;
+		}
+
+		if (placeOfSubinAndTimeQueue.front().second > destinationTime) {
+			placeOfSubinAndTimeQueue.pop();
+			break;
+		}
+
+		wayOfArriveDestination += 1;
+		placeOfSubinAndTimeQueue.pop();
+	}
+
+	return wayOfArriveDestination;
+}
+
+void initIsVistedStair() {
+	for (int i = 0; i <= MAX_NUMBER_OF_STAIR; i++) {
+		visitedStairTime[i] = 1024 * 1024 * 1024 * 2 - 1;
+	}
+}
---
+++ bfs/Baekjoon_1699_제곱수의 합.cpp
@@ -0,0 +1,43 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	1699
+	problem name	:	제곱수의 합
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+#include <queue>
+#include <utility>
+using namespace std;
+
+const int MAX_NUMBER = 100000;
+
+bool isMadeNumber[MAX_NUMBER + 1];
+
+int main() {
+	int N;
+	cin >> N;
+
+	queue<pair<int, int>> numberAndCountOfSquareNumber;
+	numberAndCountOfSquareNumber.push(make_pair(N, 0));
+
+	while (!numberAndCountOfSquareNumber.empty()) {
+		int currentNumber = numberAndCountOfSquareNumber.front().first;
+		int currentCountOfSquareNumber = numberAndCountOfSquareNumber.front().second;
+
+		if (currentNumber == 0) {
+			cout << currentCountOfSquareNumber << '\n';
+			break;
+		}
+
+		numberAndCountOfSquareNumber.pop();
+		for (int i = 1; i*i <= currentNumber; i++) {
+			int nextNumber = currentNumber - i * i;
+			if (isMadeNumber[nextNumber] == true) {
+				continue;
+			}
+			numberAndCountOfSquareNumber.push(make_pair(nextNumber, currentCountOfSquareNumber + 1));
+			isMadeNumber[nextNumber] = true;
+		}
+	}
+}
