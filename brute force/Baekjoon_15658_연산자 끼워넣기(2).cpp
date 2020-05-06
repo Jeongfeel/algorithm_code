bf0bd6e05b3dcfa4c14e8435ecbe6c20a401cffa
Jeongfeel <eve8108-@hanmail.net>
2020-05-06 13:37:19 +0900

    solved by jp.

---
+++ brute force/Baekjoon_15658_연산자 끼워넣기(2).cpp
@@ -0,0 +1,74 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	15658
+	problem name	:	연산자 끼워넣기(2)
+	algorithm	:	brute force
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countOfOperator[4];
+
+void checkMaximumAndMinimumNumber(int currentIndex, int currentNumber);
+
+int maximumNumber = -1000000000;
+int minimumNumber = 1000000000;
+
+int N;
+vector<int> numberVector;
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int number;
+		cin >> number;
+
+		numberVector.push_back(number);
+	}
+
+	for (int i = 0; i < 4; i++) {
+		cin >> countOfOperator[i];
+	}
+
+	checkMaximumAndMinimumNumber(1, numberVector[0]);
+
+	cout << maximumNumber << '\n' << minimumNumber << '\n';
+}
+
+void checkMaximumAndMinimumNumber(int currentIndex, int currentNumber) {
+	if (currentIndex == N) {
+		if (currentNumber > maximumNumber) {
+			maximumNumber = currentNumber;
+		}
+		if (currentNumber < minimumNumber) {
+			minimumNumber = currentNumber;
+		}
+	}
+	else {
+		if (countOfOperator[0] > 0) {
+			countOfOperator[0] -= 1;
+			int nextNumber = currentNumber + numberVector[currentIndex];
+			checkMaximumAndMinimumNumber(currentIndex + 1, nextNumber);
+			countOfOperator[0] += 1;
+		}
+		if (countOfOperator[1] > 0) {
+			countOfOperator[1] -= 1;
+			int nextNumber = currentNumber - numberVector[currentIndex];
+			checkMaximumAndMinimumNumber(currentIndex + 1, nextNumber);
+			countOfOperator[1] += 1;
+		}
+		if (countOfOperator[2] > 0) {
+			countOfOperator[2] -= 1;
+			int nextNumber = currentNumber * numberVector[currentIndex];
+			checkMaximumAndMinimumNumber(currentIndex + 1, nextNumber);
+			countOfOperator[2] += 1;
+		}
+		if (countOfOperator[3] > 0) {
+			countOfOperator[3] -= 1;
+			int nextNumber = currentNumber / numberVector[currentIndex];
+			checkMaximumAndMinimumNumber(currentIndex + 1, nextNumber);
+			countOfOperator[3] += 1;
+		}
+	}
+}
---
+++ brute force/Baekjoon_1759_암호 만들기.cpp
@@ -0,0 +1,71 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	1759
+	problem name	:	암호 만들기
+	algorithm	:	brute force
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void checkPossiblePassword(int count, int currentIndex, string currentPassword);
+bool isAbleCurrentPassword(string currentPassword);
+
+vector<string> possiblePasswordVector;
+
+int L, C;
+vector<char> alphabetVector;
+int main() {
+	cin >> L >> C;
+
+	for (int i = 0; i < C; i++) {
+		char alphabet;
+		cin >> alphabet;
+
+		alphabetVector.push_back(alphabet);
+	}
+
+	sort(alphabetVector.begin(), alphabetVector.end());
+	checkPossiblePassword(0, 0, "");
+
+	for (int i = 0; i < possiblePasswordVector.size(); i++) {
+		cout << possiblePasswordVector[i] << '\n';
+	}
+}
+
+void checkPossiblePassword(int count, int currentIndex, string currentPassword) {
+	if (count == L) {
+		if (isAbleCurrentPassword(currentPassword) == true) {
+			possiblePasswordVector.push_back(currentPassword);
+		}
+	}
+	else {
+		for (int i = currentIndex; i < C; i++) {
+			string nextPassword = currentPassword + alphabetVector[i];
+			checkPossiblePassword(count + 1, i + 1, nextPassword);
+		}
+	}
+}
+
+bool isAbleCurrentPassword(string currentPassword) {
+	int countOfConsonant = 0;
+	int countOfVowel = 0;
+
+	for (int i = 0; i < currentPassword.length(); i++) {
+		if (currentPassword[i] == 'a' || currentPassword[i] == 'e' || currentPassword[i] == 'i' || currentPassword[i] == 'o' || currentPassword[i] == 'u') {
+			countOfVowel += 1;
+		}
+		else {
+			countOfConsonant += 1;
+		}
+	}
+
+	if (countOfConsonant >= 2 && countOfVowel >= 1) {
+		return true;
+	}
+
+	return false;
+}
