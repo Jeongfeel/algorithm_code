aa091f1a69e00db7b9b3f8f61ea4afd708bf98f9
Jeongfeel <eve8108-@hanmail.net>
2020-05-12 14:20:24 +0900

    solved by jp. problem : 16949 / time over

---
+++ bfs/Baekjoon_16946_벽 부수고 이동하기4_시간초과.cpp
@@ -0,0 +1,140 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	16946
+	problem name	:	벽 부수고 이동하기4
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <map>
+using namespace std;
+
+const int MAX_NUMBER_OF_PLACE = 1000 + 1;
+const char BLANK = '0';
+const char WALL = '1';
+const int NOT_VISITED = 0;
+const int VISITED = -1;
+
+char place[MAX_NUMBER_OF_PLACE][MAX_NUMBER_OF_PLACE];
+int identiferPlace[MAX_NUMBER_OF_PLACE][MAX_NUMBER_OF_PLACE];
+
+int moveXCoordinate[] = { 1,-1,0,0 };
+int moveYCoordinate[] = { 0,0,1,-1 };
+
+void makeIdentifierPlace();
+void checkIdentiferPlace(int xCoordinate, int yCoordinate);
+void makeAnswer();
+
+map<int, int> identiferAndCountMap;
+
+int N, M;
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> place[i][j];
+		}
+	}
+
+	makeIdentifierPlace();
+	makeAnswer();
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << place[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void makeIdentifierPlace() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (place[i][j] == WALL) {
+				continue;
+			}
+			if (identiferPlace[i][j] != NOT_VISITED) {
+				continue;
+			}
+
+			checkIdentiferPlace(i, j);
+		}
+	}
+}
+
+void checkIdentiferPlace(int xCoordinate, int yCoordinate) {
+	static int identifer = 1;
+	int count = 0;
+	queue<pair<int, int>> checkAdjacentBlankQueue;
+
+	checkAdjacentBlankQueue.push(make_pair(xCoordinate, yCoordinate));
+
+	while (!checkAdjacentBlankQueue.empty()) {
+		int currentXCoordinate = checkAdjacentBlankQueue.front().first;
+		int currentYCoordinate = checkAdjacentBlankQueue.front().second;
+		count += 1;
+
+		identiferPlace[currentXCoordinate][currentYCoordinate] = identifer;
+		checkAdjacentBlankQueue.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nextXCoordinate = currentXCoordinate + moveXCoordinate[i];
+			int nextYCoordinate = currentYCoordinate + moveYCoordinate[i];
+
+			if (nextXCoordinate < 0 || nextXCoordinate == N || nextYCoordinate < 0 || nextYCoordinate == M) {
+				continue;
+			}
+			if (place[nextXCoordinate][nextYCoordinate] == WALL) {
+				continue;
+			}
+			if (identiferPlace[nextXCoordinate][nextYCoordinate] != NOT_VISITED) {
+				continue;
+			}
+
+			checkAdjacentBlankQueue.push(make_pair(nextXCoordinate, nextYCoordinate));
+		}
+	}
+
+	identiferAndCountMap[identifer] = count;
+	identifer += 1;
+}
+
+void makeAnswer() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (identiferPlace[i][j] == NOT_VISITED) {
+				int count = 1;
+				map<int, bool> checkSameIdentifer;
+
+				for (int k = 0; k < 4; k++) {
+					int nextXCoordinate = i + moveXCoordinate[k];
+					int nextYCoordinate = j + moveYCoordinate[k];
+
+					if (nextXCoordinate < 0 || nextXCoordinate == N || nextYCoordinate < 0 || nextYCoordinate == M) {
+						continue;
+					}
+					if (identiferPlace[nextXCoordinate][nextYCoordinate] == NOT_VISITED) {
+						continue;
+					}
+
+					int currentIdentifier = identiferPlace[nextXCoordinate][nextYCoordinate];
+					if (checkSameIdentifer[currentIdentifier] == true) {
+						continue;
+					}
+
+					checkSameIdentifer[currentIdentifier] = true;
+					count += identiferAndCountMap[currentIdentifier];
+				}
+
+				count %= 10;
+				place[i][j] = count + '0';
+			}
+			else {
+				place[i][j] = BLANK;
+			}
+		}
+	}
+}
---
+++ bfs/Baekjoon_4179_불!.cpp
@@ -0,0 +1,107 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	4179
+	problem name	:	불!
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+#include <tuple>
+#include <queue>
+using namespace std;
+
+const int MAX_LENGTH_OF_MAZE = 1001;
+const char WALL = '#';
+const char BLANK = '.';
+const char JIHOON = 'J';
+const char FIRE = 'F';
+
+char maze[MAX_LENGTH_OF_MAZE][MAX_LENGTH_OF_MAZE];
+
+int moveXCoordinate[] = { 1,-1,0,0 };
+int moveYCoordinate[] = { 0,0,1,-1 };
+
+void pushFirePosition(queue<tuple<int,int, int>> &positionQueue);
+void pushJihoonPosition(queue<tuple<int,int, int>> &positionQueue);
+void checkEscapeTime(queue<tuple<int, int, int>> positionQueue);
+
+int R, C;
+int main() {
+	cin >> R >> C;
+
+	queue<tuple<int, int, int>> positionQueue;
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> maze[i][j];
+		}
+	}
+
+	pushFirePosition(positionQueue);
+	pushJihoonPosition(positionQueue);
+	checkEscapeTime(positionQueue);
+}
+
+void pushFirePosition(queue <tuple <int, int, int>> &positionQueue) {
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			if (maze[i][j] == FIRE) {
+				positionQueue.push(make_tuple(i, j, 0));
+			}
+		}
+	}
+}
+
+void pushJihoonPosition(queue <tuple<int, int, int>> &positionQueue) {
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			if (maze[i][j] == JIHOON) {
+				positionQueue.push(make_tuple(i, j, 0));
+			}
+		}
+	}
+}
+
+void checkEscapeTime(queue<tuple<int, int, int>> positionQueue) {
+	while (!positionQueue.empty()) {
+		int currentXCoordinate = get<0>(positionQueue.front());
+		int currentYCoordinate = get<1>(positionQueue.front());
+		int currentTime = get<2>(positionQueue.front());
+
+		positionQueue.pop();
+
+		if (maze[currentXCoordinate][currentYCoordinate] == JIHOON && (currentXCoordinate == 0 || currentYCoordinate == 0 || currentXCoordinate == R - 1 || currentYCoordinate == C - 1)) {
+			cout << currentTime + 1 << '\n';
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nextXCoordinate = currentXCoordinate + moveXCoordinate[i];
+			int nextYCoordinate = currentYCoordinate + moveYCoordinate[i];
+
+			if (nextXCoordinate < 0 || nextXCoordinate == R || nextYCoordinate < 0 || nextYCoordinate == C) {
+				continue;
+			}
+
+			if (maze[nextXCoordinate][nextYCoordinate] == WALL) {
+				continue;
+			}
+
+			if (maze[currentXCoordinate][currentYCoordinate] == FIRE) {
+				if (maze[nextXCoordinate][nextYCoordinate] == BLANK) {
+					maze[nextXCoordinate][nextYCoordinate] = FIRE;
+					positionQueue.push(make_tuple(nextXCoordinate, nextYCoordinate, currentTime + 1));
+				}
+			}
+
+			if (maze[currentXCoordinate][currentYCoordinate] == JIHOON) {
+				if (maze[nextXCoordinate][nextYCoordinate] == BLANK) {
+					maze[nextXCoordinate][nextYCoordinate] = JIHOON;
+					positionQueue.push(make_tuple(nextXCoordinate, nextYCoordinate, currentTime + 1));
+				}
+			}
+		}
+	}
+
+	cout << "IMPOSSIBLE" << '\n';
+}
