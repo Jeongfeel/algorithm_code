078c1c43925f0efa56f1dbbb4287c971e6e05c17
Jeongfeel <eve8108-@hanmail.net>
2020-05-25 20:26:49 +0900

    solved by jp.

---
+++ bfs/Baekjoon_5427_불.cpp
@@ -0,0 +1,100 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	5427
+	problem name	:	불
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+const int MAX_LENGTH_OF_BUILDING = 1000;
+const char BLANK = '.';
+const char WALL = '#';
+const char SANGEUN = '@';
+const char FIRE = '*';
+
+int moveXCoordinate[] = { 1,-1,0,0 };
+int moveYCoordinate[] = { 0,0,1,-1 };
+
+char building[MAX_LENGTH_OF_BUILDING][MAX_LENGTH_OF_BUILDING];
+
+void printMinimumTimeToEscape(int w, int h);
+
+int main() {
+	int testcase;
+	cin >> testcase;
+
+	while (testcase--) {
+		int w, h;
+		cin >> w >> h;
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				cin >> building[i][j];
+			}
+		}
+
+		printMinimumTimeToEscape(h, w);
+	}
+}
+
+void printMinimumTimeToEscape(int w, int h) {
+	queue<tuple<int, int, int>> positionAndTimeQueue;
+
+	tuple<int, int, int> sangeunPosition;
+	for (int i = 0; i < w; i++) {
+		for (int j = 0; j < h; j++) {
+			if (building[i][j] == FIRE) {
+				positionAndTimeQueue.push({ i,j,0 });
+			}
+			else if (building[i][j] == SANGEUN) {
+				sangeunPosition = { i,j,0 };
+			}
+		}
+	}
+	positionAndTimeQueue.push(sangeunPosition);
+
+	while (!positionAndTimeQueue.empty()) {
+		int currentXCoordinate = get<0>(positionAndTimeQueue.front());
+		int currentYCoordinate = get<1>(positionAndTimeQueue.front());
+		int currentTime = get<2>(positionAndTimeQueue.front());
+		positionAndTimeQueue.pop();
+
+		if (building[currentXCoordinate][currentYCoordinate] == SANGEUN) {
+			if (currentXCoordinate == 0 || currentYCoordinate == 0 || currentXCoordinate == w - 1 || currentYCoordinate == h - 1) {
+				cout << currentTime + 1 << '\n';
+				return;
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nextXCoordinate = currentXCoordinate + moveXCoordinate[i];
+			int nextYCoordinate = currentYCoordinate + moveYCoordinate[i];
+
+			if (nextXCoordinate < 0 || nextXCoordinate == w || nextYCoordinate < 0 || nextYCoordinate == h) {
+				continue;
+			}
+			if (building[nextXCoordinate][nextYCoordinate] == WALL) {
+				continue;
+			}
+
+			if (building[currentXCoordinate][currentYCoordinate] == FIRE) {
+				if (building[nextXCoordinate][nextYCoordinate] == BLANK) {
+					building[nextXCoordinate][nextYCoordinate] = FIRE;
+					positionAndTimeQueue.push({ nextXCoordinate,nextYCoordinate,currentTime + 1 });
+				}
+			}
+			else if (building[currentXCoordinate][currentYCoordinate] == SANGEUN) {
+				if (building[nextXCoordinate][nextYCoordinate] == BLANK) {
+					building[nextXCoordinate][nextYCoordinate] = SANGEUN;
+					positionAndTimeQueue.push({ nextXCoordinate,nextYCoordinate,currentTime + 1 });
+				}
+			}
+		}
+	}
+	
+	cout << "IMPOSSIBLE" << '\n';
+}
---
+++ simulation/Baekjoon_16954_움직이는 미로 탈출.cpp
@@ -0,0 +1,128 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	16954
+	problem name	:	움직이는 미로 탈출
+	algorithm	:	simulation
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int LENGTH_OF_CHESS_BOARD = 8;
+const char BLANK = '.';
+const char WALL = '#';
+const char WOOKJE = 'W';
+
+int moveXCoordinate[] = { 1,-1,0,0,1,-1,1,-1 };
+int moveYCoordinate[] = { 0,0,1,-1,1,1,-1,-1 };
+
+char chessBoard[LENGTH_OF_CHESS_BOARD][LENGTH_OF_CHESS_BOARD];
+
+bool possibleToMoveDestination();
+bool isWallExisted();
+void moveWookje();
+void moveWall();
+bool isWookjeExsited();
+
+int main() {
+	for (int i = 0; i < LENGTH_OF_CHESS_BOARD; i++) {
+		for (int j = 0; j < LENGTH_OF_CHESS_BOARD; j++) {
+			cin >> chessBoard[i][j];
+		}
+	}
+
+	chessBoard[LENGTH_OF_CHESS_BOARD - 1][0] = WOOKJE;
+
+	if (possibleToMoveDestination() == true) {
+		cout << "1\n";
+	}
+	else {
+		cout << "0\n";
+	}
+}
+
+bool possibleToMoveDestination() {
+	while (isWallExisted()) {
+		moveWookje();
+		moveWall();
+	}
+
+	if (isWookjeExsited()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool isWallExisted() {
+	for (int i = 0; i < LENGTH_OF_CHESS_BOARD; i++) {
+		for (int j = 0; j < LENGTH_OF_CHESS_BOARD; j++) {
+			if (chessBoard[i][j] == WALL) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void moveWookje() {
+	vector<pair<int, int>> wookjePosition;
+
+	for (int i = 0; i < LENGTH_OF_CHESS_BOARD; i++) {
+		for (int j = 0; j < LENGTH_OF_CHESS_BOARD; j++) {
+			if (chessBoard[i][j] == WOOKJE) {
+				wookjePosition.push_back({ i,j });
+			}
+		}
+	}
+
+	for (int i = 0; i < wookjePosition.size(); i++) {
+		int currentXCoordinate = wookjePosition[i].first;
+		int currentYCoordinate = wookjePosition[i].second;
+
+		for (int j = 0; j < 8; j++) {
+			int nextXCoordinate = currentXCoordinate + moveXCoordinate[j];
+			int nextYCoordinate = currentYCoordinate + moveYCoordinate[j];
+
+			if (nextXCoordinate < 0 || nextXCoordinate == LENGTH_OF_CHESS_BOARD || nextYCoordinate < 0 || nextYCoordinate == LENGTH_OF_CHESS_BOARD) {
+				continue;
+			}
+			if (chessBoard[nextXCoordinate][nextYCoordinate] == WALL) {
+				continue;
+			}
+
+			chessBoard[nextXCoordinate][nextYCoordinate] = WOOKJE;
+		}
+	}
+}
+
+void moveWall() {
+	for (int i = 0; i < LENGTH_OF_CHESS_BOARD; i++) {
+		if (chessBoard[LENGTH_OF_CHESS_BOARD - 1][i] == WALL) {
+			chessBoard[LENGTH_OF_CHESS_BOARD - 1][i] = BLANK;
+		}
+	}
+	for (int i = LENGTH_OF_CHESS_BOARD - 1; i >= 0; i--) {
+		for (int j = 0; j < LENGTH_OF_CHESS_BOARD; j++) {
+			if (chessBoard[i][j] == WALL) {
+				chessBoard[i + 1][j] = WALL;
+				chessBoard[i][j] = BLANK;
+			}
+		}
+	}
+}
+
+bool isWookjeExsited() {
+	for (int i = 0; i < LENGTH_OF_CHESS_BOARD; i++) {
+		for (int j = 0; j < LENGTH_OF_CHESS_BOARD; j++) {
+			if (chessBoard[i][j] == WOOKJE) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
