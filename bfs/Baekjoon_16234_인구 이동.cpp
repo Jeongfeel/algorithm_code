2ce0d3972ea90f272b40f44cac20962404290c41
Jeongfeel <eve8108-@hanmail.net>
2020-06-03 15:43:59 +0900

    solved by jp.

---
+++ bfs/Baekjoon_16234_인구 이동.cpp
@@ -0,0 +1,136 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	16234
+	problem name	:	인구 이동
+	algorithm	:	bfs
+*/
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+const int MAX_LENGTH_OF_GROUND = 50;
+
+int moveXCoordinate[] = { 1,-1,0,0 };
+int moveYCoordinate[] = { 0,0,1,-1 };
+
+int ground[MAX_LENGTH_OF_GROUND][MAX_LENGTH_OF_GROUND];
+bool isVisitedGround[MAX_LENGTH_OF_GROUND][MAX_LENGTH_OF_GROUND];
+bool isMoveable = false;
+
+void printCountOfMovingPopulation();
+void initIsVisitedGround();
+void checkAdjacentGround(int xCoordinate, int yCoordinate);
+void changeGround(vector<pair<int, int>> adjacentCountryVector);
+
+int N, L, R;
+int main() {
+	cin >> N >> L >> R;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> ground[i][j];
+		}
+	}
+
+	printCountOfMovingPopulation();
+}
+
+void printCountOfMovingPopulation() {
+	int count = 0;
+
+	while (true) {
+		isMoveable = false;
+		initIsVisitedGround();
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (isVisitedGround[i][j] == true) {
+					continue;
+				}
+
+				checkAdjacentGround(i, j);
+			}
+		}
+
+		if (isMoveable == false) {
+			break;
+		}
+		count += 1;
+	}
+
+	cout << count << '\n';
+}
+
+void initIsVisitedGround() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			isVisitedGround[i][j] = false;
+		}
+	}
+}
+
+void checkAdjacentGround(int xCoordinate, int yCoordinate) {
+	queue<pair<int, int>> adjacentCountryQueue;
+	vector<pair<int, int>> adjacentCountryVector;
+
+	adjacentCountryQueue.push({ xCoordinate,yCoordinate });
+
+	while (!adjacentCountryQueue.empty()) {
+		int x = adjacentCountryQueue.front().first;
+		int y = adjacentCountryQueue.front().second;
+		adjacentCountryQueue.pop();
+		if (isVisitedGround[x][y] == true) {
+			continue;
+		}
+
+		isVisitedGround[x][y] = true;
+		adjacentCountryVector.push_back({ x,y });
+				
+		for (int i = 0; i < 4; i++) {
+			int nextX = x + moveXCoordinate[i];
+			int nextY = y + moveYCoordinate[i];
+
+			if (nextX < 0 || nextX == N || nextY < 0 || nextY == N) {
+				continue;
+			}
+			if (isVisitedGround[nextX][nextY] == true) {
+				continue;
+			}
+
+			int differenceOfPopulation = abs(ground[x][y] - ground[nextX][nextY]);
+			if (differenceOfPopulation < L || differenceOfPopulation > R) {
+				continue;
+			}
+
+			adjacentCountryQueue.push({ nextX,nextY });
+		}
+	}
+
+	if (adjacentCountryVector.size() == 1) {
+		return;
+	}
+
+	isMoveable = true;
+
+	changeGround(adjacentCountryVector);
+}
+
+void changeGround(vector<pair<int, int>> adjacentCountryVector) {
+	int sumOfPopulation = 0;
+	for (int i = 0; i < adjacentCountryVector.size(); i++) {
+		int x = adjacentCountryVector[i].first;
+		int y = adjacentCountryVector[i].second;
+
+		sumOfPopulation += ground[x][y];
+	}
+
+	sumOfPopulation /= adjacentCountryVector.size();
+	for (int i = 0; i < adjacentCountryVector.size(); i++) {
+		int x = adjacentCountryVector[i].first;
+		int y = adjacentCountryVector[i].second;
+
+		ground[x][y] = sumOfPopulation;
+	}
+}
---
+++ brute force/Baekjoon_1062_가르침.cpp
@@ -0,0 +1,96 @@
+﻿/*
+	problem site	:	baekjoon
+	problem number	:	1062
+	problem name	:	가르침
+	algorithm	:	brute force
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int COUNT_OF_ALPHABET = 26;
+
+bool isReadableAlphabet[COUNT_OF_ALPHABET];
+
+void initIsReadableAlphabet();
+void checkMaximumReadableWord(int index, int count);
+int getReadableWordCount();
+
+int maximumReadableWord = 0;
+
+int N, K;
+vector<string> wordVector;
+int main() {
+	cin >> N >> K;
+
+	if (K < 5) {
+		cout << "0\n";
+		return 0;
+	}
+	for (int i = 0; i < N; i++) {
+		string word;
+		cin >> word;
+
+		wordVector.push_back(word);
+	}
+
+	initIsReadableAlphabet();
+
+	checkMaximumReadableWord(0, 0);
+	cout << maximumReadableWord << '\n';
+}
+
+void initIsReadableAlphabet() {
+	isReadableAlphabet['a' - 'a'] = true;
+	isReadableAlphabet['n' - 'a'] = true;
+	isReadableAlphabet['t' - 'a'] = true;
+	isReadableAlphabet['i' - 'a'] = true;
+	isReadableAlphabet['c' - 'a'] = true;
+}
+
+void checkMaximumReadableWord(int index, int count) {
+	if (count == K - 5) {
+		int count = getReadableWordCount();
+
+		if (maximumReadableWord < count) {
+			maximumReadableWord = count;
+		}
+	}
+	else {
+		for (int i = index; i < COUNT_OF_ALPHABET; i++) {
+			if (isReadableAlphabet[i] == true) {
+				continue;
+			}
+
+			isReadableAlphabet[i] = true;
+			checkMaximumReadableWord(i + 1, count + 1);
+			isReadableAlphabet[i] = false;
+		}
+	}
+}
+
+int getReadableWordCount() {
+	int count = 0;
+
+	for (int i = 0; i < wordVector.size(); i++) {
+		bool isReadable = true;
+		for (int j = 0; j < wordVector[i].size(); j++) {
+			char alphabet = wordVector[i][j];			
+			if (isReadableAlphabet[alphabet - 'a'] != true) {
+				isReadable = false;
+				break;
+			}
+		}
+
+		if (isReadable == false) {
+			continue;
+		}
+
+		count += 1;
+	}
+
+	return count;
+}
